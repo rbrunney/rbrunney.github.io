83e0b79dc384942c7e219d8e135d508cabcdba3b
rbrunney <rbrunney.dev@gmail.com>
2024-01-26 19:46:46 -0700

    About Page Edits
    About page has been changed to fit better wit home page

--- rbrunney-dev/src/App.tsx
+++ rbrunney-dev/src/App.tsx
@@ -1,5 +1,4 @@
 import './styling/app.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './components/Pages/Home/Home';
 import AboutPage from './components/Pages/About/About';
 import ParticleBackgroundEffect from './components/Background/ParticleBackgroundEffect';
@@ -9,8 +8,8 @@ function App() {
   return (
     <>
       <ParticleBackgroundEffect />
-      <NavigationBar />
       <section id='home'>
+        <NavigationBar />
         <HomePage />
       </section>
       <section id='about'>
